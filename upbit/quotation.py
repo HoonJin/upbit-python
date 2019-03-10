fa409c76d720f2c8bab9dbd0d7d99f81edf30451
HoonJin <bwjhj1030@gmail.com>
2019-03-10 22:57:28 +0900

    simple done quotation

--- upbit/quotation.py
+++ upbit/quotation.py
@@ -3,11 +3,47 @@ from urllib.parse import urljoin
 
 
 class Quotation:
+    __DEFAULT_COUNT = 10
+
     def __init__(self):
-        self.host = 'https://api.upbit.com/v1/'
+        self.__host = 'https://api.upbit.com/v1/'
+        self.__headers = {}
 
     def market(self):
         return self.__get('market/all')
 
-    def __get(self, path, params: dict=None):
-        return requests.get(urljoin(self.host, path), params=params)
+    def candles_minutes(self, market: str, unit=1, to=None, count=__DEFAULT_COUNT):
+        return self.__get(f'candles/minutes/{unit}', {
+            'market': market,
+            'to': to,
+            'count': count
+        })
+
+    def trade_ticks(self, market: str, to=None, count=__DEFAULT_COUNT, cursor=None):
+        return self.__get('trades/ticks', {
+            'market': market,
+            'to': to,
+            'count': count,
+            'cursor': cursor
+        })
+
+    def ticker(self, markets: list):
+        return self.__get('ticker', {
+            'markets': markets
+        })
+
+    def orderbook(self, markets: list):
+        return self.__get('orderbook', {
+            'markets': markets
+        })
+
+    def limits(self):
+        return self.__headers['Remaining-Req']
+
+    def __get(self, path: str, params: dict=None):
+        res = requests.get(urljoin(self.__host, path), params=params)
+        if res.status_code in [200, 201]:
+            self.__headers = res.headers
+            return res.json()
+        else:
+            raise Exception("request_to_upbit_failed")
