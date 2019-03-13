ff67c55a846ead4213615370467ac4e693eacd93
HoonJin <bwjhj1030@gmail.com>
2019-03-14 01:18:03 +0900

    fix error

--- upbit/__init__.py
+++ upbit/__init__.py
@@ -4,7 +4,7 @@ from .const import *
 
 
 __quotation = Quotation()
-market = __quotation.market
+market_all = __quotation.market_all
 candles_minutes = __quotation.candles_minutes
 ticker = __quotation.ticker
 orderbook = __quotation.orderbook
